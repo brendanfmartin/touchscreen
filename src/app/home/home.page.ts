36366ee3ab6b14bf2b29da005a77d3065864a25b
brendanfmartin <brendanfmartin@gmail.com>
2020-02-09 21:16:52 -0500

    Basic click event

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  canvas: HTMLCanvasElement;
 
   constructor() {}
+
+  ngOnInit() {
+    this.canvas = document.getElementById('main') as HTMLCanvasElement;
+    this.canvas.width = document.body.clientWidth;
+    this.canvas.height = document.body.clientHeight;
+
+    document.body.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      if (this.canvas.getContext) {
+        const ctx = this.canvas.getContext('2d');
+        const X = e.x;
+        const Y = e.y;
+        const R = 10;
+        ctx.beginPath();
+        ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = '#FF0000';
+        ctx.stroke();
+      } else {
+        console.error('err');
+      }
+    }, true);
+
+  }
 
 }
