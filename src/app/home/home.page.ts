d41f1e70b7677ac555e81d0bda644cfc16c76689
Brendan Martin <bmartin@relaynetwork.com>
2020-02-11 16:00:41 -0500

    Change color. use after init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements AfterContentInit {
+
+  private color = '#46ff30';
 
   canvas: HTMLCanvasElement;
 
   constructor() {}
 
-  ngOnInit() {
+  ngAfterContentInit() {
+    this.init();
+  }
+
+  private init(): void {
     this.canvas = document.getElementById('main') as HTMLCanvasElement;
     this.canvas.width = document.body.clientWidth;
     this.canvas.height = document.body.clientHeight;
@@ -27,13 +33,12 @@ export class HomePage implements OnInit {
         ctx.beginPath();
         ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
         ctx.lineWidth = 3;
-        ctx.strokeStyle = '#FF0000';
+        ctx.strokeStyle = this.color;
         ctx.stroke();
       } else {
         console.error('err');
       }
     }, true);
-
   }
 
 }
