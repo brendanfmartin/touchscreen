78faad98e99e5d2f9a61f1a44af0e40572134f4b
Brendan Martin <bmartin@relaynetwork.com>
2020-02-11 18:43:53 -0500

    Test code for removing event listener

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -22,23 +22,60 @@ export class HomePage implements AfterContentInit {
     this.canvas.width = document.body.clientWidth;
     this.canvas.height = document.body.clientHeight;
 
-    document.body.addEventListener('click', (e) => {
-      e.preventDefault();
+    // document.body.addEventListener('mousedown', (e) => this.listener(e));
+    // document.body.removeEventListener('mouseup', (e) => this.listener(e));
 
-      if (this.canvas.getContext) {
-        const ctx = this.canvas.getContext('2d');
-        const X = e.x;
-        const Y = e.y;
-        const R = 10;
-        ctx.beginPath();
-        ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = this.color;
-        ctx.stroke();
+
+    const body = document.querySelector('body')
+    const clickTarget = document.getElementById('click-target')
+    const mouseOverTarget = document.getElementById('mouse-over-target')
+
+    let toggle = false;
+    function makeBackgroundYellow() {
+      if (toggle) {
+        body.style.backgroundColor = 'white';
       } else {
-        console.error('err');
+        body.style.backgroundColor = 'yellow';
       }
-    }, true);
+
+      toggle = !toggle;
+    }
+
+    clickTarget.addEventListener('click',
+      makeBackgroundYellow,
+      false
+    );
+
+    mouseOverTarget.addEventListener('mouseover', function () {
+      clickTarget.removeEventListener('click',
+        makeBackgroundYellow,
+        false
+      );
+    });
+
+  }
+
+  private listener(eO: MouseEvent): void {
+    eO.preventDefault();
+    document.body.addEventListener('mousemove', (e) => this.drawing(e));
+  }
+
+  private drawing(e: MouseEvent): void {
+    console.log(e);
+    e.preventDefault();
+    if (this.canvas.getContext) {
+      const ctx = this.canvas.getContext('2d');
+      const X = e.x;
+      const Y = e.y;
+      const R = 10;
+      ctx.beginPath();
+      ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = this.color;
+      ctx.stroke();
+    } else {
+      console.error('err');
+    }
   }
 
 }
